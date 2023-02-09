ffe86f7753e9885bcbb5884339d3289ce0f4cfb3
Sunny Shah <sangatshah1@gmail.com>
2023-02-09 01:21:46 -0800

    add 'Language' dropdown to Sign Up page. Fill 'Language' dropdown with available languages in google-translate api

--- gibber-web/src/components/Input/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {TextInput} from "./styles";
-
-function Input({value, onChange, placeholder, ...props}) {
-  return <TextInput value={value} onChange={e => onChange(e.target.value)} placeholder={placeholder} {...props} />
-}
-
-export default Input;
