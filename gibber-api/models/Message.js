22000a1ee419ed29f8bb0e35221807479c49775f
Sangat Shah <sangatshah1@gmail.com>
2023-02-09 01:27:39 -0800

    modify 'text' field in MessageSchema to store text in all languages for the conversation

--- gibber-api/models/Message.js
+++ gibber-api/models/Message.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const MessageSchema = new mongoose.Schema({
   conversationId: mongoose.Types.ObjectId,
   user: {type: mongoose.Types.ObjectId, ref: 'User'},
-  text: String,
+  text: [{language: String, text: String}],
   originalLang: String,
   originalText: String,
   image: String,
