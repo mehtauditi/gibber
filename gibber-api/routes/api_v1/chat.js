e73f74a0de544df9224e94a40075504482b67cff
seungdonchoi <dchoifor2@gmail.com>
2023-02-13 14:17:59 -0800

    this is for task #49

--- gibber-api/routes/api_v1/chat.js
+++ gibber-api/routes/api_v1/chat.js
@@ -97,18 +97,17 @@ const translateText = async (text, lang, tarLang) => {
   try{
     const options = {
       method: 'POST',
-      url: 'https://google-translate1.p.rapidapi.com/language/translate/v2',
+      url: 'https://nlp-translation.p.rapidapi.com/v1/translate',
       headers: {
         'content-type': 'application/x-www-form-urlencoded',
-        'Accept-Encoding': 'application/gzip',
         'X-RapidAPI-Key': process.env.RAPID_API_KEY,
-        'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com'
+        'X-RapidAPI-Host': 'nlp-translation.p.rapidapi.com'
       },
-      data: {q: text, source: lang, target: tarLang}
+      data: {text: text, from: lang, to: tarLang}
     };
-  
+
     const resp =  await axios.request(options);
-    return resp.data.data.translations[0].translatedText;
+    return resp.data.translated_text[tarLang];
   }catch(e){
     next(e);
   }
@@ -118,7 +117,7 @@ const reply = async (req, res, next) => {
   try {
     const {messageData, originalLang}  = req.body;
     const conversation = await Conversation.findOne({_id: req.params.conversation}, {users: 1, mutedBy: 1});
-    const reply = new Message({conversationId: req.params.conversation, user: req.payload.id, createdAt: new Date(), ...messageData});
+    var reply = new Message({conversationId: req.params.conversation, user: req.payload.id, createdAt: new Date(), ...messageData});
     if(messageData.text){
       let users =  conversation.users.map(u => u._id.toString());
       let userLangs = await Promise.all(users.map(async uId => {
