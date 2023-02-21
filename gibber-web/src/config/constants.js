bfd83abaf7f25762c95a932ed86cb5d47d2ad5b4
Sangat Shah <sangatshah1@gmail.com>
2023-02-20 22:55:23 -0800

    for prod

--- gibber-web/src/config/constants.js
+++ gibber-web/src/config/constants.js
@@ -1,5 +1,5 @@
 const constants = {
-  base_url: 'http://127.0.0.1:8000',
+  base_url: process.env.REACT_APP_BASE_API_URL,
   bucket_url: process.env.REACT_APP_S3_BUCKET_URL,
   maps_api: process.env.REACT_APP_GOOGLE_MAPS_API
 }
