3808d79e21b40e65df24fb1be8b616237402d323
Megh Arya <megharya.wk@gmail.com>
2023-03-08 00:30:25 -0600

    Added styled header

--- gibber-web/src/pages/ChatRoom/styles.js
+++ gibber-web/src/pages/ChatRoom/styles.js
@@ -25,3 +25,29 @@ export const Container = styled.div`
     justify-content: center;
   }
 `;
+
+export const Header = styled.header`
+  width: 100%;
+  height: 5%;
+  position: relative;
+  z-index: 2;
+  border-radius: 25px;
+  background-color: ${({theme}) => theme.mode === 'dark' ? theme.bg : '#fff'};
+  display: flex;
+  overflow: hidden;
+  audio::-webkit-media-controls-panel {
+    background-color: ${({theme}) => theme.bg2};
+  }
+  audio::-webkit-media-controls-play-button {
+    background-color: #B1D4E0;
+    border-radius: 50%;
+    margin-right: 5px;
+  }
+  audio::-webkit-media-controls-current-time-display, audio::-webkit-media-controls-time-remaining-display {
+    color: ${({theme}) => theme.txt};
+  }
+  .loading {
+    width: 100%;
+    justify-content: center;
+  }
+`;
