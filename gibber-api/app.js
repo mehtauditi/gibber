48d2806c5215dba7f324c4cbf81ed042a0362fb6
Sangat Shah <sangatshah1@gmail.com>
2023-03-16 12:13:42 -0700

    changes for dev mode

--- gibber-api/app.js
+++ gibber-api/app.js
@@ -19,9 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors({
-  origin: process.env.ALLOWED_CORS_ORIGINS.split(',') // 'http://localhost:3000'
-}));
+app.use(cors({}));
 
 
 const mongoUri = process.env.MONGO_URI;
