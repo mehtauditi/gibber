828b710b24c03cbd4e9b02083e358829d5c2dc0a
John Kim <joki9448@gmail.com>
2023-03-23 18:56:33 -0400

    created imagemessage component and added rendermessageimage attribute to giftedchat component

---
+++ gibber-app/src/components/ImageMessage/index.js
@@ -0,0 +1,24 @@
+import React, {useEffect, useState} from 'react';
+import Api from "../../../config/axios";
+import {ImgMessage} from '../styles';
+
+function ImageMessage({src}) {
+    
+  const [url, setUrl] = useState();
+
+  useEffect(async () => {
+    const data =  await Api.post('/chat/getFile', {path: src});
+    setUrl(data.data.msg);
+  }, [src, url])
+
+  return (
+    <div style={{marginBottom: 15}}>
+      <ImgMessage
+        src={url}
+        controls
+      />
+    </div>
+  )
+}
+
+export default ImageMessage;
