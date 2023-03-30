fbe23256c0ca0b55ce1731e319df4571cf1fbe4f
Sangat Shah <sangatshah1@gmail.com>
2023-03-30 11:41:36 -0700

    add .env vars and podfile.lock

--- gibber-app/src/config/constants.js
+++ gibber-app/src/config/constants.js
@@ -1,5 +1,9 @@
+import Config from "react-native-config"
+
+
 export default {
   base_url: 'http://localhost:8000',
-  bucket_url: 'https://gibber-chat-main.s3.amazonaws.com/',
-  onesignal_app: '6ff38dd1-6346-47ed-a05d-ae3da96eaf65' // app id
+  bucket_url: Config.REACT_APP_S3_BUCKET_URL,
+  onesignal_app: Config.REACT_APP_ONE_SIGNAL_APP_ID, // app id
+  translate_api: Config.REACT_APP_GOOGLE_TRANSLATE_API_KEY
 }
