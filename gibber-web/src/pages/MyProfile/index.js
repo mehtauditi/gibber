acf296e848ef0dcac57d5fc22144810ef2bf2245
Megh Arya <megharya.wk@gmail.com>
2023-04-11 18:05:35 -0500

    Styled myProfile.

--- gibber-web/src/pages/MyProfile/index.js
+++ gibber-web/src/pages/MyProfile/index.js
@@ -3,21 +3,28 @@ import { NavLink, useLocation } from "react-router-dom";
 import {theme} from "../../config/theme";
 import {ThemeProvider} from "styled-components";
 import { useOutletContext} from "react-router-dom";
-import { CenteredContent, Button, Logo } from "../../utils/sharedStyles";
-import {AvatarButton, CustomCheckbox, Container, Divider, TextField} from "./styles";
+import { CenteredContent, Button, Logo, Row } from "../../utils/sharedStyles";
+import {CustomCheckbox, Container, Divider, TextField, ProfileHeader, ProfileForm} from "./styles";
 import PasswordChecklist from 'react-password-checklist';
 import Api from "../../config/axios";
 import "./index.css";
 import { toast } from "react-toastify";
 import FileUpload from "../../components/FileUpload";
-import { RowItem} from "../../components/Chat/styles";
 import {getAvatarPath} from "../../utils/helpers";
 import {useOutsideAlerter} from "../../utils/useOutsideAlerter";
+import {Icon, Switch} from "../../components";
+import DropdownInput from "../../components/DropdownInput";
+import {languages} from "../../utils/languages";
 
 function MyProfile(props) {
   const location = useLocation();
   const userData = location.state;
-  const [checkBoxValue, setCheckBoxValue] = useState(false);
+
+  // This state variable will keep track of whether the component is in edit mode or not,
+  // if true user is allowed to edit the profile
+  const [isEditMode, setIsEditMode] = useState(false);
+
+  const [toggle, setToggle] = useState(userData.translateUser);
   const [avatar, setAvatar] = useState(getAvatarPath(userData.avatar));
   const [username, setUsername] = useState(userData.name);
   const [password, setPassword] = useState({
@@ -36,7 +43,7 @@ function MyProfile(props) {
       return;
     }
     if (username !== userData.name) {
-        await Api.put(`/user`, { name: username } );
+      await Api.put(`/user`, { name: username } );
     }
     if (!password.newPassword && !password.confirmPassword && !password.currentPassword) {
       toast.success('Profile Updated');
@@ -62,12 +69,20 @@ function MyProfile(props) {
 
   };
 
+  const toggleEditMode = () => {
+    setIsEditMode(!isEditMode);
+    if(isEditMode){
+      handleProfileUpdateSubmit();
+    }
+  }
+
   const handleChangeUserName = (event) => {
     setUsername(event.target.value);
   };
 
   const handleCheckboxChange = async (event) => {
-    setCheckBoxValue(event.target.checked);
+    console.log(event.target.checked);
+    setToggle(event.target.checked);
     await Api.put(`/user/translateUser`, { translateUser: event.target.checked } );
   };
 
@@ -76,7 +91,7 @@ function MyProfile(props) {
     setPassword({ ...password, [name]: value });
   };
 
-const handleAvatarChange = (source) => {
+  const handleAvatarChange = (source) => {
     const uri = URL.createObjectURL(source);
     let data = {['image']: uri};
     const formData = new FormData();
@@ -90,103 +105,125 @@ const handleAvatarChange = (source) => {
         .catch((error) => {
           toast.error("Error");
         });
-}
+  }
 
-useEffect(async () => {
-  let imag = await getAvatarPath(userData.avatar);
-  setAvatar(imag);
-}, [userData.avatar])
+  const nullFunction = () => {
+    toast.warn('Sorry language change is not available yet')
+  }
+
+  useEffect(async () => {
+    let imag = await getAvatarPath(userData.avatar);
+    setAvatar(imag);
+  }, [userData.avatar])
 
   return (
-    <ThemeProvider theme={mode === 'dark' ? theme.dark : theme.light}>
-    <Container>
-      <div style={{padding: '20px'}}>
-        <NavLink to="/app/chat" className="back-to-chat">
-          Return To Chat
-        </NavLink>
-      </div>
-      <CenteredContent>
-          <FileUpload accept="image/*" onChange={handleAvatarChange}>
-            <RowItem>
-                <AvatarButton src={avatar}/>
-            </RowItem>
-          </FileUpload>
-
-        <h2 style={{ marginTop: "20px", marginBottom: '5px' }}>Account Information</h2>
-        <Divider style={{width: '250px', marginBottom: 20}}/>
-        <div className="user-container">
-          <h4 style={{ display: "inline-block" }}>Display Name:</h4>
-          <TextField style={{ display: "inline-block" }}>
-            <input
-              style={{ display: "inline-block" }}
-              value={username}
-              onChange={handleChangeUserName}
-            ></input>
-          </TextField>
-          <div className="email-info">
-            <h4>Email Address:</h4>
-            <h5 style={{ display: "inline-block" }}>{userData.email}</h5>
-          </div>
-          <div className="phone-info">
-            <h4>Phone Number:</h4>
-            <h5 style={{ display: "inline-block" }}>{!userData.phone ? 'None' : userData.phone}</h5>
-          </div>
-          <div className="language-info">
-            <h4>Language:</h4>
-            <h5 style={{ display: "inline-block" }}>{userData.language}</h5>
-            <div>
-              <h6 style={{marginLeft: '20px'}}>
-              Translate my messages
-              <CustomCheckbox style={{blockSize:'15px', marginLeft:'10px'}} type="checkbox" onChange={handleCheckboxChange} defaultChecked={userData.translateUser}/>
-            </h6>
+      <ThemeProvider theme={mode === 'dark' ? theme.dark : theme.light}>
+        <ProfileHeader>
+          <NavLink to="/app/chat" className="back-to-chat" style={{alignSelf:"flex-start"}}>
+            <Icon style={{position:'fixed'}}name="arrow-back-outline" size={40} color={'gray'}/>
+          </NavLink>
+        </ProfileHeader>
+        <Container>
+          <CenteredContent style={{width:'50%'}}>
+            <div className='avatar-container'>
+                <div className='outer'>
+                  <img src={avatar} className='image' style={{height: '200px', width: '200px', borderRadius:"50%"}}/>
+                  <div className='inner'>
+                    <FileUpload accept="image/*" onChange={handleAvatarChange}>
+                      <Icon name="edit-outline" size={25} color={'white'}/>
+                    </FileUpload>
+                  </div>
+                </div>
             </div>
-          </div>
-        </div>
-        <h2 style={{marginBottom: '5px'}}>Change Password</h2>
-        <Divider style={{width: '250px', marginBottom: 20}}/>
-        <div className="password-container">
-          <TextField>
-            <h4>Current Password: </h4>
-            <input
-              type="password"
-              value={password.currentPassword}
-              name="currentPassword"
-              onChange={handlePasswordChange}
-            ></input>
-          </TextField>
-          <TextField>
-            <h4>New Password:</h4>
-            <input
-              type="password"
-              value={password.newPassword}
-              name="newPassword"
-              onChange={handlePasswordChange}
-            ></input>
-            <PasswordChecklist
-                rules={["minLength", "number","capital"]}
-                minLength={8}
-                value={password.newPassword}
-            />
-            <br />
-            <h4>Confirm Password:</h4>
-            <input
-              type="password"
-              value={password.confirmPassword}
-              name="confirmPassword"
-              onChange={handlePasswordChange}
-            ></input>
-          </TextField>
-          <Button
-            className="update-profile"
-            onClick={handleProfileUpdateSubmit}
-          >
-            Update Profile
-          </Button>
-        </div>
-      </CenteredContent>
-    </Container>
-    </ThemeProvider>
+            <h1>Account Information</h1>
+            {isEditMode ? (
+                <ProfileForm className="form-container" style={{width:'100%'}}>
+                  <div className='element-container'>
+                    <h3 className='element-label'>Name</h3>
+                    <TextField>
+                      <input type='text' name='username' placeholder={userData.name} onChange={handleChangeUserName}/>
+                    </TextField>
+                  </div>
+                  <hr style={{height:'2px',color:'gray', background:'gray'}}/>
+                  <div className='element-container' style={{paddingTop:'0px'}}>
+                    <h3 className='element-label' style={{paddingTop:'35px'}}>Language</h3>
+                    <DropdownInput onChange={nullFunction}/>
+                  </div>
+                  <div className='element-container' style={{paddingTop:'15px', justifyContent:'left', marginLeft:'25px'}}>
+                    <h4 className='element-label' style={{paddingRight:'35px', paddingTop:'10px'}}>Translate my messages</h4>
+                      <div className="form-check form-switch" style={{display:'flex'}}>
+                        <input
+                              className="form-check-input"
+                              checked={Boolean(toggle)}
+                              type="checkbox"
+                              role="switch"
+                              id="flexSwitchCheckDefault"
+                              onChange={handleCheckboxChange}
+                              style={{display:'inline-flex', width:'40px', height:'20px'}}
+                          />
+                      </div>
+                  </div>
+                  <hr style={{height:'2px',color:'gray', background:'gray'}}/>
+                  <h2 className='element-label'>Change Password</h2>
+                  <div className='element-container'>
+                    <h3 className='element-label'>Current Password</h3>
+                    <TextField>
+                      <input type='password' value={password.currentPassword} name='currentPassword' onChange={handlePasswordChange}/>
+                    </TextField>
+                  </div>
+                  <div className='element-container'>
+                    <h3 className='element-label'>New Password</h3>
+                    <TextField>
+                      <input type='password' value={password.newPassword} name='newPassword' onChange={handlePasswordChange}/>
+                    </TextField>
+                  </div>
+                  <div className='element-container'>
+                    <PasswordChecklist
+                        rules={["minLength", "number","capital"]}
+                        minLength={8}
+                        value={password.newPassword}
+                        style={{marginLeft:'58%'}}
+                    />
+                    <br />
+                  </div>
+                  <div className='element-container'>
+                    <h3 className='element-label'>Confirm Password</h3>
+                    <TextField>
+                      <input type='password' value={password.confirmPassword} name='confirmPassword' onChange={handlePasswordChange}/>
+                    </TextField>
+                  </div>
+                  <div style={{display:'flex', paddingTop:'25px', justifyContent:'center'}}>
+                    <button className="rounded-button" onClick={toggleEditMode}>{isEditMode ? 'Save' : 'Edit'}</button>
+                  </div>
+                </ProfileForm>
+            ):(
+                <ProfileForm className="form-container" style={{width:'75%'}}>
+                  <div className='element-container'>
+                    <h2 className='element-label' style={{color:'gray'}}>Name</h2>
+                    <h2 className='element-label' >{userData.name}</h2>
+                  </div>
+                  <hr style={{height:'2px',color:'gray', background:'gray'}}/>
+                  <div className='element-container'>
+                    <h2 className='element-label' style={{color:'gray'}}>Email</h2>
+                    <h2 className='element-label'>{userData.email}</h2>
+                  </div>
+                  <hr style={{height:'2px',color:'gray', background:'gray'}}/>
+                  <div className='element-container'>
+                    <h2 className='element-label' style={{justifyContent:'left', color:'gray'}}>Phone</h2>
+                    <h2 className='element-label'>{userData.phone}</h2>
+                  </div>
+                  <hr style={{height:'2px',color:'gray', background:'gray'}}/>
+                  <div className='element-container'>
+                    <h2 className='element-label' style={{justifyContent:'left', color:'gray'}}>Language</h2>
+                    <h2>{(languages.find(value => value.language === userData.language)).name}</h2>
+                  </div>
+                  <div style={{display:'flex', paddingTop:'25px', justifyContent:'center'}}>
+                    <button className="rounded-button" onClick={toggleEditMode}>{isEditMode ? 'Save' : 'Edit'}</button>
+                  </div>
+                </ProfileForm>)}
+          </CenteredContent>
+        </Container>
+      </ThemeProvider>
   );
 }
-
 export default MyProfile;
--- gibber-web/src/pages/MyProfile/styles.js
+++ gibber-web/src/pages/MyProfile/styles.js
@@ -1,18 +1,20 @@
 import styled from "styled-components";
+import SwitchComp from "react-switch";
 
 export const Container = styled.div`
-    height: 100%;
-    width: 100%;
-    padding: 0px;
-    margin: 0px;
-    display: flex;
-    flex-direction: column;
-    border-radius: 25px;
-    align-items: center;
-    overflow-y: scroll;
-    background-color: ${props => props.theme.bg};
-    color: ${props => props.theme.txt};
-    position: relative;
+  height: 100%;
+  width: 100%;
+  padding: 0px;
+  margin: 0px;
+  display: flex;
+  flex-direction: column;
+  border-color: gray;
+  border-radius: 0px 0px 25px 25px;
+  align-items: center;
+  overflow-y:  scroll;
+  background-color: ${props => props.theme.bg};
+  color: ${props => props.theme.txt};
+  position: relative;
 `
 
 export const Divider = styled.div`
@@ -28,7 +30,7 @@ export const TextField = styled.div`
 
     margin-left: 15px;
 
-    & h4 {
+    & h5 {
         display: inline-block;
         text-align: left;
         margin: 5px 5px 5px 0px;
@@ -40,14 +42,40 @@ export const TextField = styled.div`
     }
 `;
 
-export const AvatarButton = styled.img`
-  height: 175px;
-  width: 175px;
+export const AvatarDisplay = styled.img`
+  height: 200px;
+  width: 200px;
   border-radius: 50%;
   border: 2px solid grey;
+  margin-top: 25px;
 `;
 
 export const CustomCheckbox = styled.input`
     margin-left: 10px;
     margin-right: 10px;
+`;
+
+export const ProfileHeader = styled.header`
+  width: 100%;
+  position: relative;
+  z-index: 2;
+  border-radius: 25px 25px 0px 0px;
+  background-color: ${({theme}) => theme.bg};
+  display: flex;
+  transition: all 0.3s ease-in;
+  overflow: hidden;
+  .loading {
+    width: 100%;
+    justify-content: center;
+  }
+`;
+
+export const ProfileForm = styled.div`
+  max-width: 100%;
+  margin-bottom: 50px;
+  padding: 50px;
+  background-color: ${({theme}) => theme.bg};
+  border: none;
+  border-radius: 25px;
+  box-shadow: lightgray 0px 0px 5px;;
 `;
